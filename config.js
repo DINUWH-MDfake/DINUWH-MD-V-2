a78ded2bc887b197115d7c7340c9a94154c020cd
DINUWH-MD <dinethnethruvan772@gmail.com>
2024-12-30 17:07:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG, || "https://i.ibb.co/mcWTCp2/3586.jpg"
+ALIVE_MSG: process.env.ALIVE_IMG, || "*හායි ලස්සන ළමයෝව් DINUWH-MD බොට් වෙතව සාදරෙයෙන් පිලිගන්නේ නෑ🥲*"
 };
